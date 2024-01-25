b0b69cd0195e3a8124834f8ff9bc2a6cd9ce227f
jay suthar <tirth4134@gmail.com>
2024-01-25 10:25:59 +0530

    #

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,15 +1,15 @@
-import { Schema, Types, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IEvent extends Document {
   _id: string;
   title: string;
   description?: string;
   location?: string;
-  createAt: Date;
+  createdAt: Date;
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  Price?: string;
+  price: string;
   isFree: boolean;
   url?: string;
   category: { _id: string; name: string };
@@ -20,11 +20,11 @@ const EventSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
   location: { type: String },
-  createAt: { type: String, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
   imageUrl: { type: String, required: true },
   startDateTime: { type: Date, default: Date.now },
   endDateTime: { type: Date, default: Date.now },
-  Price: { type: String },
+  price: { type: String },
   isFree: { type: Boolean, default: false },
   url: { type: String },
   category: { type: Schema.Types.ObjectId, ref: "Category" },
